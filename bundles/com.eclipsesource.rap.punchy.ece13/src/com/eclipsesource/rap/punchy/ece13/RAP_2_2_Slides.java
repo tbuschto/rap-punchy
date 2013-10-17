4dadd967494471bb0e66f40d99d48fbf1d990a8d
Tim Buschtöns <tbuschto@eclipsesource.com>
2013-10-17 16:00:44 +0200

    Add ToolTip slide

--- bundles/com.eclipsesource.rap.punchy.ece13/src/com/eclipsesource/rap/punchy/ece13/RAP_2_2_Slides.java
+++ bundles/com.eclipsesource.rap.punchy.ece13/src/com/eclipsesource/rap/punchy/ece13/RAP_2_2_Slides.java
@@ -2,21 +2,36 @@ package com.eclipsesource.rap.punchy.ece13;
 
 import static com.eclipsesource.rap.punchy.ece13.ResourceLoaderUtil.readFile;
 
+import org.eclipse.jface.fieldassist.ControlDecoration;
+import org.eclipse.jface.fieldassist.FieldDecoration;
+import org.eclipse.jface.fieldassist.FieldDecorationRegistry;
+import org.eclipse.jface.viewers.ArrayContentProvider;
+import org.eclipse.jface.viewers.ColumnLabelProvider;
+import org.eclipse.jface.viewers.ColumnViewerToolTipSupport;
+import org.eclipse.jface.viewers.TableViewer;
+import org.eclipse.jface.viewers.TableViewerColumn;
 import org.eclipse.rap.rwt.lifecycle.WidgetUtil;
 import org.eclipse.rap.rwt.scripting.ClientListener;
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.events.FocusAdapter;
-import org.eclipse.swt.events.FocusEvent;
 import org.eclipse.swt.graphics.Color;
+import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Canvas;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Event;
 import org.eclipse.swt.widgets.Listener;
+import org.eclipse.swt.widgets.ProgressBar;
+import org.eclipse.swt.widgets.Scale;
+import org.eclipse.swt.widgets.TabFolder;
+import org.eclipse.swt.widgets.TabItem;
+import org.eclipse.swt.widgets.Table;
 import org.eclipse.swt.widgets.Text;
+import org.eclipse.swt.widgets.ToolBar;
+import org.eclipse.swt.widgets.ToolItem;
 
 import com.eclipsesource.rap.punchy.Presentation;
 
@@ -156,6 +171,76 @@ public class RAP_2_2_Slides {
         flow( "transparent", parent );
       }
     };
+    new EceSlide( presentation ) {
+      @Override
+      public String getTitle() {
+        return "New ToolTips";
+      }
+      @Override
+      protected void createContent( Composite slideComposite ) {
+        super.createContent( slideComposite );
+        spacer( 20 );
+        list(
+          "New (default) look",
+          "Widget-specific behavior"
+        );
+        Composite parent = new Composite( slideComposite, SWT.NONE );
+        GridLayout layout = new GridLayout( 3, false );
+        layout.verticalSpacing = 10;
+        layout.horizontalSpacing = 10;
+        parent.setLayout( layout );
+        ToolBar toolBarLeft = new ToolBar( parent, SWT.VERTICAL | SWT.BORDER );
+        GridData layoutData = new GridData( SWT.DEFAULT, SWT.FILL, false, true );
+        layoutData.verticalSpan = 6;
+        toolBarLeft.setLayoutData( layoutData );
+        fillToolBar( toolBarLeft );
+        ToolBar toolBarTop = new ToolBar( parent, SWT.HORIZONTAL | SWT.BORDER );
+        GridData layoutData1 = new GridData( SWT.LEFT, SWT.DEFAULT, false, false );
+        toolBarTop.setLayoutData( layoutData1 );
+        fillToolBar( toolBarTop );
+        createTabFolder( parent );
+        createButton( parent, "Button" );
+        styleAs( "transparent", createCheck( parent ) );
+        createText( parent );
+        ProgressBar bar = new ProgressBar( parent, SWT.HORIZONTAL );
+        bar.setSelection( 40 );
+        bar.setToolTipText( "hor" );
+        addBarListener( bar );
+        Scale scale = new Scale( parent, SWT.HORIZONTAL );
+        scale.setToolTipText( "Scale" );
+        styleAs( "transparent", scale );
+        addScaleListener( scale );
+        flow( "transparent", parent );
+      }
+    };
+  }
+
+  //////////////
+  // Helper
+
+  private static void fillToolBar( ToolBar toolbarTop ) {
+    createToolItem( toolbarTop, "ICON_ERROR" );
+    createToolItem( toolbarTop, "ICON_INFORMATION" );
+    createToolItem( toolbarTop, "ICON_QUESTION" );
+    createToolItem( toolbarTop, "ICON_WARNING" );
+    createToolItem( toolbarTop, "ICON_WORKING" );
+  }
+
+  private static void createToolItem( ToolBar toolbar, String icon ) {
+    try {
+      int id = SWT.class.getField( icon ).getInt( null );
+      ToolItem item = new ToolItem( toolbar, SWT.CHECK );
+      item.setImage( Display.getCurrent().getSystemImage( id ) );
+      item.setToolTipText( icon );
+    } catch( IllegalArgumentException e ) {
+      e.printStackTrace();
+    } catch( SecurityException e ) {
+      e.printStackTrace();
+    } catch( IllegalAccessException e ) {
+      e.printStackTrace();
+    } catch( NoSuchFieldException e ) {
+      e.printStackTrace();
+    }
   }
 
   private static void addFocusSwitchExample( Composite parent ) {
@@ -224,18 +309,121 @@ public class RAP_2_2_Slides {
     return button;
   }
 
-  private static void addDateValidator( final Text text ) {
-    text.addFocusListener( new FocusAdapter() {
-      Color color = new Color( text.getDisplay(), 255, 128, 128 );
+
+  private static void createTabFolder( Composite composite ) {
+    TabFolder folder = new TabFolder( composite, SWT.NONE );
+    GridData layoutData = new GridData( SWT.FILL, SWT.FILL, true, true );
+    layoutData.verticalSpan = 6;
+    folder.setLayoutData( layoutData );
+    TabItem tabItem1 = new TabItem( folder, SWT.NONE );
+    tabItem1.setText( "Create" );
+    tabItem1.setToolTipText( "ToolTips" );
+    TabItem tabItem2 = new TabItem( folder, SWT.NONE );
+    tabItem2.setText( "Edit" );
+    tabItem2.setToolTipText( "appear" );
+    TabItem tabItem3 = new TabItem( folder, SWT.NONE );
+    tabItem3.setText( "Publish" );
+    tabItem3.setToolTipText( "quickliy..." );
+    tabItem1.setControl( createTableViewer( folder ) );
+    folder.setSelection( 0 );
+  }
+
+  private static Control createTableViewer( Composite composite ) {
+    TableViewer table = new TableViewer( composite );
+    table.getControl().setToolTipText( "Table" );
+    GridData layoutData = new GridData( SWT.FILL, SWT.FILL, true, true );
+    //layoutData.horizontalSpan = 3;
+    table.getControl().setLayoutData( layoutData );
+    table.setContentProvider( new ArrayContentProvider() );
+    ColumnViewerToolTipSupport.enableFor( table );
+    ( ( Table )table.getControl() ).setHeaderVisible( true );
+    TableViewerColumn columnViewer = new TableViewerColumn( table, SWT.NONE );
+    columnViewer.getColumn().setWidth( 100 );
+    columnViewer.getColumn().setToolTipText( "a column" );
+    columnViewer.getColumn().setText( "Col 1" );
+    columnViewer.setLabelProvider( new ColumnLabelProvider(){
       @Override
-      public void focusLost( FocusEvent event ) {
-        if( !verifyDate( text.getText() ) ) {
-          text.setBackground( color );
-        } else {
-          text.setBackground( null );
+      public String getToolTipText(Object element) {
+        return "More about: " + element.toString();
+      };
+      @Override
+      public String getText(Object element) {
+        return element.toString();
+      };
+    } );
+    TableViewerColumn columnViewerTwo = new TableViewerColumn( table, SWT.NONE );
+    columnViewerTwo.getColumn().setWidth( 170 );
+    columnViewerTwo.getColumn().setText( "Col 2" );
+    columnViewerTwo.setLabelProvider( new ColumnLabelProvider(){
+      @Override
+      public String getToolTipText(Object element) {
+        return "Even more about " + element.toString();
+      };
+      @Override
+      public String getText(Object element) {
+        return "Another " + element.toString();
+      };
+    } );
+    TableViewerColumn columnViewer3= new TableViewerColumn( table, SWT.NONE );
+    columnViewer3.getColumn().setWidth( 100 );
+    columnViewer3.getColumn().setText( "Col 3" );
+    columnViewer3.setLabelProvider( new ColumnLabelProvider() );
+    table.setInput( new String[]{ "Data One ", "Data Two", "Data Three" } );
+    return table.getControl();
+  }
+
+  private static void addScaleListener( Scale scale ) {
+    String script =   "function handleEvent( event ) { "
+                    + "  event.widget.setToolTipText( event.widget.getSelection() + \"%\" ); "
+                    + "}";
+    scale.addListener( SWT.Selection, new ClientListener( script ) );
+  }
+
+  private static void addBarListener( final ProgressBar bar ) {
+    bar.addListener( SWT.MouseDown, new Listener() {
+      @Override
+      public void handleEvent( Event event ) {
+        int sel = bar.getSelection();
+        sel += 10;
+        if( sel > bar.getMaximum() ) {
+          sel = 0;
         }
+        bar.setSelection( sel );
+        bar.setToolTipText( sel + "%" );
       }
     } );
+  }
+
+  private static Button createButton( Composite composite, String text ) {
+    Button ok = new Button( composite, SWT.PUSH );
+    ok.setText( text );
+    ok.setToolTipText( "This is OK!" );
+    return ok;
+  }
+
+  private static Button createCheck( Composite composite ) {
+    Button check = new Button( composite, SWT.CHECK );
+    check.setText( "check this out" );
+    check.setToolTipText( "yohoo!" );
+    return check;
+  }
+
+  private static Text createText( Composite composite ) {
+    Text text = new Text( composite, SWT.BORDER );
+    text.setText( "Text with text in the text" );
+    text.setToolTipText( "I have nothing to say" );
+    ControlDecoration decoration = new ControlDecoration( text, SWT.RIGHT | SWT.TOP );
+    decoration.setDescriptionText( "Deco" );
+    decoration.setMarginWidth( 2 );
+    Image warningImage = getDecorationImage( FieldDecorationRegistry.DEC_WARNING );
+    decoration.setImage( warningImage );
+    return text;
+  }
+
+  private static Image getDecorationImage( String id ) {
+    FieldDecorationRegistry registry = FieldDecorationRegistry.getDefault();
+    FieldDecoration decoration = registry.getFieldDecoration( id );
+    return decoration.getImage();
   }
 
   private static boolean verifyDate( String date ) {
